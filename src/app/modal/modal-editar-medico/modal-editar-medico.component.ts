33eeaba3be3def2073cf70cf34e9a4f757bb21a2
FLCampa <flcampanelli@gmail.com>
2021-11-27 16:25:41 -0300

    implementa componente de conteúdo do modal de editar médico

--- src/app/modal/modal-editar-medico/modal-editar-medico.component.ts
+++ src/app/modal/modal-editar-medico/modal-editar-medico.component.ts
@@ -1,15 +1,15 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'app-modal-editar-medico',
   templateUrl: './modal-editar-medico.component.html',
-  styleUrls: ['./modal-editar-medico.component.css']
+  styleUrls: ['./modal-editar-medico.component.css'],
 })
 export class ModalEditarMedicoComponent implements OnInit {
+  formEditarMedico: FormGroup;
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
